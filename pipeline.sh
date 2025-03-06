d6895f846530f07a8d9f01454f36710e77bf9fc7
jtomori <jtomori@users.noreply.github.com>
2025-03-06 23:19:59 +0100

    Update comments, gitignore

--- pipeline.sh
+++ pipeline.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+# Sane defaults for bash
 set -o nounset
 set -o errexit
 trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
 set -o errtrace
 set -o pipefail
 
-# GME -> audio files
+# GME -> OGG audio files
 tttool media -d ogg 'gme/Mein Woerter-Bilderbuch Unser Zuhause.gme'
 
