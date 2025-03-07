999cd4adde45b14e7d384e17190f5363d915a30a
jtomori <jtomori@users.noreply.github.com>
2025-03-07 10:20:24 +0100

    Download gme, crate folders in pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -7,6 +7,12 @@ trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
 set -o errtrace
 set -o pipefail
 
+# Create directories
+mkdir gme ogg
+
+# Download testing GME file
+wget -c -O 'gme/Mein Woerter-Bilderbuch Unser Zuhause.gme' https://ravensburger.cloud/rvwebsite/rvDE/db/applications/Mein%20Woerter-Bilderbuch%20Unser%20Zuhause.gme
+
 # GME -> OGG audio files
 tttool media -d ogg 'gme/Mein Woerter-Bilderbuch Unser Zuhause.gme'
 
